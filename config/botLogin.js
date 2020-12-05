0d787ab6f8dfe6740dbbc8c74d24d4e1744dd224
DarkGekyume <62268013+DarkGekyume@users.noreply.github.com>
2020-12-05 17:06:51 -0300

    Add files via upload

---
+++ config/botLogin.js
@@ -0,0 +1,5 @@
+module.exports = {
+	token: "NzAwMjQ4NDQ3ODQzOTU4Nzg0.XpgLLw.VoiAej0VPuVXQUaC_g15NUVnmc0",
+	youtubeAPI: "AIzaSyA5U04qdTd9sV8le268DEIWHq5fKZpwiGY",
+	owner_id: "700248447843958784"
+}
