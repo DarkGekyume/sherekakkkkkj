c13884cbb116eee185fe484218ff823f668d3e4c
DarkGekyume <62268013+DarkGekyume@users.noreply.github.com>
2020-12-05 17:15:23 -0300

    Update botLogin.js

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "NzAwMjQ4NDQ3ODQzOTU4Nzg0.XpgLLw.VoiAej0VPuVXQUaC_g15NUVnmc0",
+	token: "NzAwMjQ4NDQ3ODQzOTU4Nzg0.XpgLLw.29bs8H6MHJOaYSyMLHLHQMK0Lig",
 	youtubeAPI: "AIzaSyA5U04qdTd9sV8le268DEIWHq5fKZpwiGY",
-	owner_id: "700248447843958784"
+	owner_id: "446468696433623040"
 }
